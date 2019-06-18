a38b5f40c45fc0d2f654578e5c8a5148d26a1a0e
Eileen Hengel <eileenhengel@Bobs-MacBook-Pro.local>
2019-06-17 21:55:55 -0400

    Update last_refresh

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -13,6 +13,9 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text )
 
+#capture last response
+last_refresh = parsed_response["Meta Data"]['3. Last Refreshed']
+
 breakpoint()
 
 def get_stock_price(stock):
@@ -36,7 +39,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refresh}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
