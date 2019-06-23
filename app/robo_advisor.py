2dd3e94b826abcbbf32290c633c375e724ea7c2a
Eileen Hengel <eileenhengel@Bobs-MacBook-Pro.local>
2019-06-23 10:46:43 -0500

    Final Submit

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -5,9 +5,10 @@ import datetime
 import json
 import csv
 import os
+import statistics
 from dotenv import load_dotenv
 
-#fine time
+#find time
 now = datetime.datetime.now()
 
 #define functions
@@ -26,8 +27,8 @@ symbol_list = []
 while True:
     #ask user for stock symbol
     symbol = input("Please input a product company stock symbol:")
-    #request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={my_cred}"
-    #response = requests.get(request_url)
+    symbol = symbol.upper()
+
     if symbol == "DONE":
         break
     elif symbol.isalpha() and len(symbol) < 6:
@@ -36,7 +37,9 @@ while True:
     else:
         print("Input must be A-Z characters only and less than or equal to 5 characters")
 
-#breakpoint()
+    #breakpoint()
+
+x = 0
 
 #iterate through symbols
 for s in symbol_list:
@@ -44,14 +47,22 @@ for s in symbol_list:
     response = requests.get(request_url)
     parsed_response = json.loads(response.text)
 
+    x = x + 1
+
     #check to see if request url returns an error message
     valid_symbol = str(parsed_response.keys())
 
     if valid_symbol == "dict_keys(['Error Message'])":
-        print(f"{s} is not a valid stock symbol. Exiting...")
-        break
+        if x == len(symbol_list):
+            print(f"{s} is not a valid stock symbol. Exiting...")
+            exit()
+        else:
+            print(f"{s} is not a valid stock symbol. Continuing...")
+        continue
     else:
         pass
+
+    #breakpoint()
 
     tsd = parsed_response['Time Series (Daily)']
 
@@ -78,6 +89,7 @@ for s in symbol_list:
     recent_high = max(highs)
     recent_low = min(lows)
 
+    #write to file
     filename = f"{s}.csv"
     csv_file_path = os.path.join(os.path.dirname(__file__),"..", "data", filename) #try to add multiple inputs later
     
@@ -96,19 +108,28 @@ for s in symbol_list:
                 "volume": daily_prices["5. volume"]
             })
 
+    #recommendation engine
+    if float(last_close) < statistics.median(highs):
+        rec = "BUY!"
+        reason = "THE CLOSING PRICE IS LOWER THAN THE MEDIAN PRICE FOR THE LAST 100 DAYS."
+    else:
+        rec = "DO NOT BUY!"
+        reason = "THE CLOSING PRICE IS HIGHER THAN THE MEDIAN PRICE FOR THE LAST 100 DAYS."
+
     print("-------------------------")
     print(f"SELECTED SYMBOL: {s}")
     print("-------------------------")
     print("REQUESTING STOCK MARKET DATA...")
-    print("RUN AT: " + str(now.strftime("%Y-%m-%d %H:%M %p"))) #fix to have name of month
+    print("RUN AT: " + str(now.strftime("%B %d, %Y %H:%M %p")))
     print("-------------------------")
     print(f"LATEST DAY: {last_refresh}")
     print(f"LATEST CLOSE: {to_usd(float(last_close))} ")
     print(f"RECENT HIGH: {to_usd(float(recent_high))}")
     print(f"RECENT LOW: {to_usd(float(recent_low))}")
+    print(f"100-DAY MEDIAN: {to_usd(statistics.median(highs))}")
     print("-------------------------")
-    print("RECOMMENDATION: BUY!") 
-    print("RECOMMENDATION REASON: TODO")
+    print(f"RECOMMENDATION: {rec}") 
+    print(f"RECOMMENDATION REASON: {reason}")
     print("-------------------------")
     print(f"WRITING DATA TO CSV... {csv_file_path} ")
     print("-------------------------")
